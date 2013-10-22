26f2b9f1a907e88e782762b3b4fd155ceae8e01f
GotoLink <gotolinkminecraft@gmail.com>
2013-10-22 20:55:34 +0200

    Reorganized stalactite blocks

--- wildCaves/BlockStoneStalactite.java
+++ wildCaves/BlockStoneStalactite.java
@@ -1,78 +1,23 @@
 package wildCaves;
 
-import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.Icon;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockStoneStalactite extends BlockStalactite
-{
-	@SideOnly(Side.CLIENT)
-	private Icon[] iconArray;
-	
-	public BlockStoneStalactite(int id, boolean solidStalactites, boolean damageWhenFallenOn) 
-	{
-        super(id, solidStalactites, damageWhenFallenOn);
-        setUnlocalizedName("stoneStalactiteBlock");
+public class BlockStoneStalactite extends BlockStalactite {
+	public BlockStoneStalactite(int id, boolean solidStalactites, boolean damageWhenFallenOn) {
+		super(id, solidStalactites, damageWhenFallenOn);
+		setUnlocalizedName("stoneStalactiteBlock");
+		setTextureName(":stoneStructure");
 	}
 
 	@Override
-    public int idDropped(int metadata, Random random, int par3)
-    {
-    	return Block.cobblestone.blockID;
-    }
-    
-	@Override
-    public int quantityDropped(Random rand)
-    {
-        return rand.nextInt(3)-1;
-    }
-    
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerIcons(IconRegister iconRegister) 
-	{
-        this.iconArray = new Icon[getNumOfStructures()];
-
-        for (int i = 0; i < this.iconArray.length; ++i)
-        {
-            this.iconArray[i] = iconRegister.registerIcon(WildCaves.modid + ":stoneStructure"+ i);
-        }
+	public int idDropped(int metadata, Random random, int par3) {
+		return Block.cobblestone.blockID;
 	}
-	
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getIcon(int side, int metadata)
-    {
-    	if(metadata >= getNumOfStructures())
-    		metadata = getNumOfStructures() -1;
-        return this.iconArray[metadata];
-    }
 
-    @Override
-	public int getNumOfStructures() 
-	{
-		return super.getNumOfStructures();
+	@Override
+	public int quantityDropped(Random rand) {
+		return rand.nextInt(3) - 1;
 	}
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getBlockTexture(IBlockAccess blockAccess, int x, int y, int z, int side)
-    {
-    	return this.iconArray[blockAccess.getBlockMetadata(x, y, z)];
-    }
 }
