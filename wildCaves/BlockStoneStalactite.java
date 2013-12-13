d5f390ce87a1266570302f6ef4b27238f4b93582
GotoLink <gotolinkminecraft@gmail.com>
2013-12-13 02:15:34 +0100

    Added dripping water particles

--- wildCaves/BlockStoneStalactite.java
+++ wildCaves/BlockStoneStalactite.java
@@ -2,7 +2,10 @@ package wildCaves;
 
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.world.World;
 
 public class BlockStoneStalactite extends BlockStalactite {
 	public BlockStoneStalactite(int id) {
@@ -19,5 +22,24 @@ public class BlockStoneStalactite extends BlockStalactite {
 	@Override
 	public int quantityDropped(Random rand) {
 		return rand.nextInt(3) - 1;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+		int l;
+		if (world.getBlockMetadata(x, y, z) < 4) {
+			boolean isWatered = world.getBlockMaterial(x, y + 2, z).isLiquid();
+			int h = y;
+			while (world.getBlockId(x, h, z) == this.blockID) {
+				if (random.nextInt(5 + (isWatered ? 0 : 10)) == 0) {
+					double d0 = x + random.nextFloat();
+					double d2 = z + random.nextFloat();
+					double d1 = h + 0.05D + (d0 - x) * (d2 - z);
+					world.spawnParticle("dripWater", d0, d1, d2, 0.0D, 0.0D, 0.0D);
+				}
+				h--;
+			}
+		}
 	}
 }
